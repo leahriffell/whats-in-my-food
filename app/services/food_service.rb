d837617d75edd4b4d30e85a8729b6cdbd9575d54
Leah Riffell <leah.riffell@gmail.com>
2020-10-19 10:15:59 -0600

    Add food service connection

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,4 +1,17 @@
 class FoodService
   def self.by_ingredient(ingredient)
+    conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods/search?query=#{URI.encode(ingredient)}") do |faraday|
+      faraday.headers["X-Api-Key"] = ENV['FOODS_API_KEY']
+    end
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
   end
+
+  private
+
+  # def self.conn
+  #   Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods") do |faraday|
+  #     faraday.headers["X-Api-Key"] = ENV['FOODS_API_KEY']
+  #   end
+  # end
 end
